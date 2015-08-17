5490be59f26d985db5b64d2258c02f4da12203d4
kjhenner <kjhenner@gmail.com>
2015-08-17 13:43:37 -0700

    add quest and state fixtures

---
+++ spec/fixtures/quests/welcome/welcome_spec.rb
@@ -0,0 +1,17 @@
+describe "Task 1:" do
+  it 'Use puppet -V to check the puppet version' do 
+    file('/root/.bash_history').should contain 'puppet -V'
+  end
+end
+
+describe "Task 2:" do
+  it 'View the options for the quest tool' do
+    file('/root/.bash_history').should contain "quest --help"
+  end
+end
+
+describe "Task 3:" do
+  it 'Check the quest progress' do 
+    file('/root/.bash_history').should contain "quest --progress"
+  end
+end
