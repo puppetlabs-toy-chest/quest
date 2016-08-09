7fcb1d75a0ad2beefdc3b721166a5fb2d6b83f4d
kjhenner <kjhenner@gmail.com>
2016-08-09 16:47:48 -0700

    remove file state

--- lib/quest/messenger.rb
+++ lib/quest/messenger.rb
@@ -1,27 +1,26 @@
 # -*- encoding : utf-8 -*-
 module Quest
 
+  # Shared state and methods for reading from the content directory
   class Messenger
 
     require 'fileutils'
 
-    attr_reader :state_dir
-    attr_reader :pidfile
-    attr_reader :task_dir
-    attr_reader :active_quest_file
-    attr_reader :status_line_file
-    attr_reader :quest_index_file
-    attr_reader :spec_helper
+    attr_reader   :quest_index_file
+    attr_reader   :spec_helper
+    attr_accessor :active_quest
 
     def initialize(config = {})
-      @state_dir = config['state_dir'] || '/var/opt/quest'
-      @pidfile   = config['pidfile']   || '/var/run/quest.pid'
       @task_dir  = config['task_dir']  || Dir.pwd
-      @active_quest_file = File.join(@state_dir, 'active_quest')
-      @status_line_file  = File.join(@state_dir, 'active_quest_status')
-      @quest_lock        = File.join(@state_dir, 'quest.lock')
+      validate_task_dir
       @quest_index_file  = File.join(@task_dir, 'index.json')
       @spec_helper       = File.join(@task_dir, 'spec_helper.rb')
+      @quest_status = {}
+      @active_quest = quests.first
+    end
+
+    def set_raw_status(quest, raw_status_hash)
+      @quest_status[quest] = raw_status_hash
     end
 
     def validate_task_dir
@@ -33,15 +32,8 @@ module Quest
       end
     end
 
-    def set_active_quest(quest)
-      File.open(@active_quest_file, 'w') do |f|
-        f.write(quest)
-      end
-      run_setup_command
-    end
-
-    def run_setup_command
-      if setup_command
+    def run_setup_command(quest)
+      if setup_command(quest)
         begin
           puts "Setting up the #{active_quest} quest..."
           Dir.chdir(@task_dir){
@@ -57,41 +49,7 @@ module Quest
       end
     end
 
-    def set_lock
-      FileUtils.touch(@quest_lock)
-    end
-
-    def release_lock
-      File.delete(@quest_lock)
-    end
-
-    def lock_on?
-      File.exist?(@quest_lock)
-    end
-
-    def offer_bailout(message)
-      print "#{message} Continue? [Y/n]:"
-      raise "Cancelled" unless [ 'y', 'yes', ''].include? STDIN.gets.strip.downcase
-    end
-
-    def active_quest_complete?
-      raw_status["summary"]["failure_count"] == 0
-    end
-
-    def set_first_quest
-      first_quest = quests.first
-      set_active_quest(first_quest)
-    end
-
-    def initialize_state_dir
-      FileUtils.mkdir_p @state_dir
-    end
-
-    def read_json(path)
-      JSON.parse(File.read(path))
-    end
-
-    def active_quest_spec_path
+    def quest_spec_path(quest)
       File.join(@task_dir, "#{active_quest}_spec.rb")
     end
 
@@ -99,79 +57,8 @@ module Quest
       read_json(@quest_index_file).keys
     end
 
-    def active_quest
-      set_first_quest unless File.exists?(@active_quest_file)
-      File.read(@active_quest_file)
-    end
-
-    def active_quest_json_output_path
-      File.join(@state_dir, "#{active_quest}.json")
-    end
-
-    def status_line_output_path
-      File.join(@state_dir, "active_quest_status")
-    end
-
-    def quest_watch
-      read_json(@quest_index_file)[active_quest]["watch_list"]
-    end
-
     def setup_command
       read_json(@quest_index_file)[active_quest]["setup_command"]
-    end
-
-    def raw_status
-      read_json(File.join(@state_dir, "#{active_quest}.json"))
-    end
-
-    def status( options = {:brief => false, :color => true, :raw => false } )
-      return raw_status if options[:raw]
-
-      quest_name = options[:color] ? active_quest.cyan : active_quest
-
-      output = "Quest: " + quest_name
-
-      if options[:brief]
-        total = raw_status["summary"]["example_count"]
-        complete = total - raw_status["summary"]["failure_count"]
-        output << " - Progress: #{complete} of #{total} Tasks."
-      else
-        # Add line break after quest name for full output
-        output << "\n"
-        raw_status["examples"].each do |example|
-          if example["status"] == "passed"
-            output << '√ '.green
-          else
-            output << 'X '.yellow
-          end
-          output << example["full_description"] + "\n"
-        end
-      end
-
-      output
-    end
-
-    def pid
-      begin
-        File.read(@pidfile).to_i
-      rescue
-        puts "The quest service isn't running. Use the questctl command to start the service."
-        raise
-      end
-    end
-
-    def send_reset
-      Process.kill("HUP", pid)
-    end
-
-    def send_quit
-      Process.kill("QUIT", pid)
-    end
-
-    def change_quest(quest)
-      set_active_quest(quest)
-      send_reset
-      puts "You are now on the " + active_quest.cyan + " quest."
     end
 
   end
--- lib/quest/quest_watcher.rb
+++ lib/quest/quest_watcher.rb
@@ -6,111 +6,28 @@ module Quest
 
     include Quest::SpecRunner
 
-    def initialize(messenger, daemonize=false)
+    def initialize(messenger)
       @messenger = messenger
-      @daemonize = daemonize
-      @pidfile = messenger.pidfile
       @timers = Timers::Group.new
-    end
-
-    def restart_watcher
-      if @watcher
-        @watcher.pause
-        Quest::LOGGER.info("Watcher paused")
-        @watcher.finalize
-        Quest::LOGGER.info("Watcher finalized pending runs")
-        @watcher.filenames = @messenger.quest_watch
-        Quest::LOGGER.info("Watcher file names set to #{@messenger.quest_watch}")
-        @watcher.resume
-        Quest::LOGGER.info("Watcher resumed")
-      else
-        Quest::LOGGER.info("No watcher instance found. Skipping watcher restart.")
-      end
-    end
-
-    def write_pid
-      begin
-        File.open(@pidfile, File::CREAT | File::EXCL | File::WRONLY){|f| f.write("#{Process.pid}") }
-        Quest::LOGGER.info("PID written to #{@pidfile}")
-        at_exit { File.delete(@pidfile) if File.exists?(@pidfile) }
-      rescue Errno::EEXIST
-        check_pid
-        retry
-      end
-    end
-
-    def check_pid
-      case pid_status
-      when :running, :not_owned
-        puts "The quest watcher is already running. Check #{@pidfile}"
-        exit 1
-      when :dead
-        File.delete(@pidfile)
-      end
-    end
-
-    def pid_status
-      return :exited unless File.exists?(@pidfile)
-      pid = File.read(@pidfile).to_i
-      return :dead if pid == 0
-      Process.kill(0, pid)
-      :running
-    rescue Errno::ESRCH
-      :dead
-    rescue Errno::EPERM
-      :not_owned
-    end
-
-    def trap_signals
-      trap(:HUP) do
-        restart_watcher
-      end
-      Quest::LOGGER.info("Trap for HUP signal set")
-    end
-
-    def start_watcher
-      Quest::LOGGER.info('Starting initial spec run')
-      test_current_quest_and_write_output
-      Quest::LOGGER.info("Initializing watcher watching for changes in #{quest_watch}")
-      @watcher = FileWatcher.new(quest_watch)
-      @watcher_thread = Thread.new(@watcher) do |watcher|
-        watcher.watch do |changed_file_path|
-          Quest::LOGGER.info("Watcher triggered by a change to #{changed_file_path}")
-          test_current_quest_and_write_output
-        end
-      end
     end
 
     def start_timer
       task_timer = @timers.now_and_every(5) do
-        unless @messenger.lock_on?
-          @messenger.set_lock
-          test_current_quest_and_write_output
-          @messenger.release_lock
-        end
+        check_active_quest
       end
       loop {@timers.wait}
     end
 
-    def test_current_quest_and_write_output
-      spec_output_hash = run_spec(@messenger.active_quest_spec_path)
-      @messenger.write_json_output(spec_output_hash, @messenger.active_quest_json_output_path)
-      @messenger.write_status_line(@messenger.status_line_output_path)
+    def check_active_quest
+      quest = @messenger.active_quest
+      raw_status = run_spec(@messenger.spec_path(quest))
+      @messenger.set_raw_status(quest, spec_output_hash)
     end
 
     # This is the main function to set up and run the watcher process
     def run!
-      if @daemonize
-        check_pid
-        Process.daemon
-      end
-      write_pid
-      trap_signals
       load_helper
       start_timer
-      # Keep a sleeping thread to handle signals.
-      thread = Thread.new { sleep }
-      thread.join
     end
 
   end
